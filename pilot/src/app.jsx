c24db8d749e829f89e4afca1dfcbd162def243f6
Gabitojr <cbjgsevilla@gmail.com>
2024-05-15 11:13:42 -0600

    removing firebase

--- pilot/src/app.jsx
+++ pilot/src/app.jsx
@@ -4,20 +4,7 @@ import Box from './components/box'
 import SearchBar from './components/searchbar/searchbar'
 import './app.css'
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCQc0QQTNpH8MmMNwKoFqTAm6aQ_h5qTZc",
-  authDomain: "pilot-7cfb9.firebaseapp.com",
-  projectId: "pilot-7cfb9",
-  storageBucket: "pilot-7cfb9.appspot.com",
-  messagingSenderId: "896697198172",
-  appId: "1:896697198172:web:7e760dec2555b7b80f2899"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 export const SearchContext = createContext(null)
 
